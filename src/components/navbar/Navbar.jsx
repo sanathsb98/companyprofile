349b26f35e193e098d2cb427c9cb1871b0499739
sanathsb98 <itssanathsb@gmail.com>
2024-01-10 11:48:40 +0530

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import '../navbar/Navbar.css';
 import logo from '/src/images/logo.svg';
+import {motion} from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const Navbar = () => {
+
+  const interactiveDivRef = useRef(null);
+
+    const animation = {
+        initial : {y : "-60%"},
+        enter: i => ({y: "20%", transition: {duration: 1.95, ease: [0.33, 1, 0.68, 1],  delay: 0.075 * i}})
+    }
+    const { ref, inView, entry } = useInView({
+      threshold: 0.75,
+      triggerOnce: true
+    });
+
   return (
     <div className='navbar'>
-       <div className='navbar-content'>
+       <motion.div ref={ref} variants={animation} initial="initial" animate={inView ? "enter" : ""} className='navbar-content'>
        <div className='nav-left'>
           <img src={logo} width='100%' height='70%'/>
         </div>
@@ -19,7 +33,7 @@ const Navbar = () => {
             <div className='nav-button'>Let's talk</div>
             <div className='nav-collapse'><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 5H11" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3 12H16" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3 19H21" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>
         </div>
-       </div>
+       </motion.div>
     </div>
   )
 }
