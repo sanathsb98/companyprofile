7d8cfb8b02281021769139908837ccd90caa4f4d
sanathsb98 <itssanathsb@gmail.com>
2024-01-10 12:36:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import './App.css';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import Lenis from '@studio-freight/lenis'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
   return (
     <>
